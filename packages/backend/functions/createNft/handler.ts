7bae2ccb06c03981f94e6e0386c751647f9b5e63
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-09-18 17:18:20 +0200

    :sparkles: createNft put nft in dynamodb

--- packages/backend/functions/createNft/handler.ts
+++ packages/backend/functions/createNft/handler.ts
@@ -1,4 +1,33 @@
+import {
+  DynamoDBClient,
+  PutItemCommand,
+  UpdateTableCommand,
+} from '@aws-sdk/client-dynamodb';
+import crypto from 'crypto';
+
+const randomIntFromInterval = (min: number, max: number) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const client = new DynamoDBClient({ region: 'eu-west-1' });
+
 export const main = (event: any) => {
   console.log('📆 event create nft', event);
-  return event;
+
+  const id = crypto.randomUUID();
+
+  const Item = {
+    PK: { S: 'Nft' }, // Partition key, S for string
+    SK: { S: id },
+    id: { S: id },
+    positionX: { N: randomIntFromInterval(5, 90).toString() }, // N for number
+    positionY: { N: randomIntFromInterval(10, 90).toString() }, // QUESTION: why do we need to convert to string ?
+    imageIndex: { N: Math.floor(Math.random() * 5).toString() },
+  };
+
+  client.send(
+    new PutItemCommand({ TableName: process.env.NFT_TABLE_NAME, Item }),
+  );
+
+  const { positionX, positionY, imageIndex } = Item;
+  return { id, positionX, positionY, imageIndex };
 };
