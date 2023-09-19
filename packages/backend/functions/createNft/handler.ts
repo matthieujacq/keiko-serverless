d49ad4ae0e7af988b69622ebba6b8b5fe5c23457
Matthieu Jacq <67386567+matthieujacq@users.noreply.github.com>
2023-09-19 11:19:09 +0200

    :bug: fix bad post request format

--- packages/backend/functions/createNft/handler.ts
+++ packages/backend/functions/createNft/handler.ts
@@ -10,14 +10,17 @@ export const main = async (event: any) => {
   console.log('📆 event create nft', event);
 
   const id = crypto.randomUUID();
+  const positionX = randomIntFromInterval(5, 90).toString();
+  const positionY = randomIntFromInterval(10, 90).toString();
+  const imageIndex = Math.floor(Math.random() * 5).toString();
 
   const Item = {
     PK: { S: 'Nft' }, // Partition key, S for string
     SK: { S: id },
     id: { S: id },
-    positionX: { N: randomIntFromInterval(5, 90).toString() }, // N for number
-    positionY: { N: randomIntFromInterval(10, 90).toString() }, // QUESTION: why do we need to convert to string ?
-    imageIndex: { N: Math.floor(Math.random() * 5).toString() },
+    positionX: { N: positionX }, // N for number
+    positionY: { N: positionY }, // QUESTION: why do we need to convert to string ?
+    imageIndex: { N: imageIndex },
   };
 
   // QUESTION: for some reason if I don't use await here, the item is created but our lambda does not return data ({ id, positionX, positionY, imageIndex }})
@@ -25,6 +28,5 @@ export const main = async (event: any) => {
     new PutItemCommand({ TableName: process.env.NFT_TABLE_NAME, Item }),
   );
 
-  const { positionX, positionY, imageIndex } = Item;
   return { id, positionX, positionY, imageIndex };
 };
